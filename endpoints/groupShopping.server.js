38c5759d0f6b73099123a7c88667857f273764f2
Kevin Oberluggauer <kevin.oberluggauer@gmail.com>
2023-10-04 22:13:17 +0200

    updated group function endpoints to async code; fixed bugs leading to disfunctions for new users.

--- endpoints/groupShopping.server.js
+++ endpoints/groupShopping.server.js
@@ -1,188 +1,183 @@
 'use strict';
 
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
 
 let file = '';
 let group = {};
 let joinedGroup = {};
 
-endpoints.add('/api/v1/groupShoppingLists', (request, response, session) => {
+endpoints.add('/api/v1/groupShoppingLists', async (request, response, session) => {
   if(!['GET', 'HEAD'].includes(request.method)){
-    response.statusCode = 405;
-    response.end();
-    return;
+    return 405;
   }
 
   if(!session.profile){
-    response.statusCode = 403;
-    response.end();
-    return;
+    return 403;
   }
 
   if(session.profile.groups.length < 1){
-    response.statusCode = 400;
-    response.end();
-    return;
+    return 400;
   }
 
   joinedGroup = session.profile.groups[0];
   file = `config/groups/${joinedGroup.groupname}/${joinedGroup.groupname}.json`;
-  fs.readFile(file, (error, data) => {
-    if(error){
-      response.statusCode = 500;
-      response.end();
-      return;
-    }
+  try{
+    const data = await fs.readFile(file, 'utf8');
+    group = await JSON.parse(data);
+  } catch(error){
+    console.log('Fehler beim parsen der Datei.');
+    return 500;
+  };
+  console.log(group)
 
+  if(joinedGroup.groupname === group.groupname &&  joinedGroup.id === group.id){
+
+    file = `config/groups/${joinedGroup.groupname}/shoppingLists.json`;
+    let errorFile =
+    [
+      {
+        "id": "6681fad1-94b3-4eb1-921d-44240d7280a6",
+        "title": "Beispiel Einkaufsliste",
+        "entries":
+        [
+          {
+            "id": "e31fd65c-1426-4fde-9efb-66ca1e40dd40",
+            "text": "Beispiel Eintrag",
+            "listId": "6681fad1-94b3-4eb1-921d-44240d7280a6"
+          }
+        ]
+      }
+    ];
+    let data;
     try{
-      group = JSON.parse(data);
-    } catch(error){
-      console.log('Fehler beim parsen der Datei.');
-    }
-    console.log('passt')
-
-    if(joinedGroup.groupname === group.groupname &&  joinedGroup.id === group.id){
-      file = `config/groups/${joinedGroup.groupname}/shoppingLists.json`;
-      fs.readFile(file, (error, data) => {
-        if(error){
-          console.log('no file')
-          return;
+      data = await fs.readFile(file, 'utf8');
+      console.log(data)
+    } catch(error) {
+      await fs.writeFile(file, JSON.stringify(errorFile, null, 2));
+      console.log('Datei erstellt.');
+      response.end(JSON.stringify(
+      [
+        {
+          "id": "6681fad1-94b3-4eb1-921d-44240d7280a6",
+          "title": "Beispiel Einkaufsliste",
+          "entries":
+          [
+            {
+              "id": "e31fd65c-1426-4fde-9efb-66ca1e40dd40",
+              "text": "Beispiel Eintrag",
+              "listId": "6681fad1-94b3-4eb1-921d-44240d7280a6"
+            }
+          ]
         }
-      console.log('aui')
-      response.end(data);
-      });
+      ]
+      ));
     };
-  });
+    response.end(data);
+  };
 });
 
-endpoints.add(`/api/v1/groupShoppingLists/:id`, (request, response, session, match) => {
-  console.log('request')
+endpoints.add(`/api/v1/groupShoppingLists/:id`, async (request, response, session, match) => {
   joinedGroup = session.profile.groups[0];
   if(!['POST', 'DELETE'].includes(request.method)){
-    response.statusCode = 405;
-    response.end();
-    return;
+    return 405;
   }
 
   if(!session.profile){
-    response.statusCode = 403;
-    response.end();
-    return;
+    return 403;
   }
 
   if(session.profile.groups.length < 1){
-    response.statusCode = 400;
-    response.end();
-    return;
+    return 400;
   }
 
   const size = parseInt(request.headers['content-length']);
   if (request.method === 'POST' && isNaN(size) || size > 1000) {
-    response.statusCode = 413;
-    response.end('413 Payload Too Large');
-    return;
+    return 413;
   }
-
+  let shoppingLists = [];
   if(joinedGroup.groupname === group.groupname &&  joinedGroup.id === group.id){
-    console.log('passtiii', joinedGroup)
     file = `config/groups/${joinedGroup.groupname}/shoppingLists.json`;
-
-    fs.readFile(file, 'utf8', (error, data) => {
-    let shoppingLists = [];
-
-    if(!error){
+    try{
+      const data = await fs.readFile(file, 'utf8');
+      shoppingLists = JSON.parse(data);
+    } catch(error) {
+      console.log('Fehler beim Parsen der Einkaufslisten:', error);
       try{
-        shoppingLists = JSON.parse(data);
-      }
-      catch(error){
-        console.log('Fehler beim Parsen der Einkaufslisten:', error);
-        response.statusCode = 500;
-        response.end();
-        return;
-      }
+        await fs.writeFile(file, JSON.stringify(shoppingLists, null, 2));
+      } catch(error) {
+        console.log('Fehler beim erstellen der Datei.', error);
+        return 500;
+      };
+    };
+  };
+
+  const index = shoppingLists.findIndex((shoppingList) => shoppingList.id === match.pathname.groups.id);
+
+  if(request.method === 'DELETE'){
+    if(index === -1){
+      return 404;
     }
 
-    if(error){
-      fs.writeFile(file, JSON.stringify(shoppingLists, null, 2), (error) => {
-        if(error){
-          console.log('Fehler beim erstellen der Datei.', error);
-          response.statusCode = 500;
-          response.end('500, Internal server error.');
-          return;
-        }
-        return;
-      })
+    shoppingLists.splice(index, 1);
+
+    try{
+      await fs.writeFile(file, JSON.stringify(shoppingLists, null, 2));
+      response.end();
+      return 204;
+    } catch(error) {
+      response.end();
+      return 500;
     }
+  };
 
-    const index = shoppingLists.findIndex((shoppingList) => shoppingList.id === match.pathname.groups.id);
-
-    if(request.method === 'DELETE'){
-      if(index === -1){
-        response.statusCode = 404;
-        response.end();
-        return;
-      }
-
-      shoppingLists.splice(index, 1);
-
-      fs.writeFile(file, JSON.stringify(shoppingLists, null, 2), (error) => {
-        console.log(2)
-        if(error){
-          response.statusCode = 500;
-          response.end();
-          return;
-        }
-
-        response.statusCode = 204;
-        response.end();
+  let chunks = [];
+  let shoppingList;
+  try{
+      shoppingList = await new Promise((resolve, reject) => {
+      request.on('data', (chunk) => chunks.push(chunk));
+      request.on('end', () => {
+        resolve(JSON.parse(Buffer.concat(chunks)));
       });
-      return;
-    }
-
-    let body = '';
-    request.on('data', (chunk) => body += chunk);
-    request.on('end', () => {
-      let shoppingList;
-
-      try{
-        shoppingList = JSON.parse(body);
-
-        if(typeof shoppingList !== 'object') throw 'lists are no objects';
-
-        if(shoppingList.id === undefined) throw 'missing id';
-        if(
-          typeof shoppingList.id !== 'string' || !shoppingList.id.match(/^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/)
-        ) throw 'invalid id';
-
-        if(shoppingList.entries === undefined) throw 'missing text key';
-        //if (typeof shoppingList.text !== 'array') throw 'invalid text key';
-        if (shoppingList.entries.length > 512) throw 'text key is too long';
-
-        if (Object.keys(shoppingList).length !== 3) throw 'too many keys';
-      }
-      catch(error){
-        console.log('Fehler beim parsen der Einkauflisten', error);
-        response.statusCode = 400;
-        response.end();
-        return;
-      }
-
-      if (index === -1) {
-        shoppingLists.push(shoppingList);
-      } else {
-        shoppingLists[index] = shoppingList;
-      }
-
-      fs.writeFile(file, JSON.stringify(shoppingLists, null, 2), (error) => {
-        if(error){
-          response.statusCode = 500;
-          response.end('Fehler');
-          return;
-        }
-
-        response.statusCode = 204;
-        response.end();
+      request.on('error', (error) => {
+        reject(error);
       });
     });
-  })};
+  } catch(error) {
+    console.log(error);
+    return 500;
+  };
+
+  try{
+    if(typeof shoppingList !== 'object') throw 'lists are no objects';
+
+    if(shoppingList.id === undefined) throw 'missing id';
+    if(
+      typeof shoppingList.id !== 'string' || !shoppingList.id.match(/^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/)
+    ) throw 'invalid id';
+
+    if(shoppingList.entries === undefined) throw 'missing text key';
+    //if (typeof shoppingList.text !== 'array') throw 'invalid text key';
+    if (shoppingList.entries.length > 512) throw 'text key is too long';
+
+    if (Object.keys(shoppingList).length !== 3) throw 'too many keys';
+  }
+  catch(error){
+    console.log('Fehler beim parsen der Einkauflisten', error);
+    return 400;
+  }
+
+  if (index === -1) {
+    shoppingLists.push(shoppingList);
+  } else {
+    shoppingLists[index] = shoppingList;
+  }
+
+  try{
+    await fs.writeFile(file, JSON.stringify(shoppingLists, null, 2));
+  } catch (error){
+    console.log(error);
+    return 500;
+  }
+  response.statusCode = 204;
+  response.end();
 });
