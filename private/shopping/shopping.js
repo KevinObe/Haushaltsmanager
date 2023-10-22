24ba38692f2a9b6b03cecd84f47a7ebfa6c75dab
Kevin Oberluggauer <kevin.oberluggauer@gmail.com>
2023-10-22 18:05:59 +0200

    updated shopping files with the Server Send Events functions.

--- private/shopping/shopping.js
+++ private/shopping/shopping.js
@@ -17,7 +17,7 @@ const $navBtn = document.querySelector('.navBtn');
 const $entries = [];
 
 let shoppingLists = [];
-
+let joinedGroup = {};
 /**************************************************************************************************/
 /** RUNTIME                                                                                      **/
 /** Declare additial variables for the application in this section.                              **/
@@ -29,6 +29,58 @@ let shoppingLists = [];
 /** FUNCTIONS                                                                                    **/
 /** Put the main logic of the application in functions and declare them in this section.         **/
 /**************************************************************************************************/
+//check group for server send events
+async function checkGroup() {
+  try{
+    const response = await fetch('/api/v1/checkGroup');
+    joinedGroup = await response.json();
+    // if(response.status === 200){
+    // };
+    if(response.status === 200){
+      await fetch(`/api/v1/live/${joinedGroup.id}`);
+    }
+  }catch(error){
+    console.log(error);
+  };
+};
+const sse = new EventSource('/api/v1/live');
+
+function receiveMessage({ data }) {
+  const message = JSON.parse(data);
+
+  if(message.type === 'online' && message.group.id === joinedGroup.id){
+    console.log(message.group.id)
+    const online = message.info;
+    $alertText.textContent = `${online}`;
+    console.log(message)
+    customAlert();
+    return;
+  }
+
+  if(message.type === 'online' && message.group.id === false){
+    console.log(message)
+    const online = message.info;
+    $alertText.textContent = `${online}`;
+    console.log(message)
+    customAlert();
+    return;
+  }
+
+//info messages todo
+if(message.type === 'ToDo' && message.group.id === joinedGroup.id){
+  $alertText.textContent = `${message.info}`;
+  customAlert();
+  return;
+}
+
+//info messages shopping
+if(message.type === 'shoppingList' && message.group.id === joinedGroup.id){
+$alertText.textContent = `${message.info}`;
+customAlert();
+return;
+}
+};
+
 function loadLists(){
   const request = new XMLHttpRequest();
 
@@ -171,9 +223,10 @@ function openList(shoppingList){
 /**************************************************************************************************/
 $addButton.addEventListener('click', addNewList);
 $navBtn.addEventListener('click', () => window.location.href = '../home.html');
-
+sse.addEventListener('message', receiveMessage);
 /**************************************************************************************************/
 /** SETUP                                                                                        **/
 /** If there are any additional steps to take in order to prepare the app, so use this section.  **/
 /**************************************************************************************************/
 loadLists();
+checkGroup();
--- private/shopping/shoppinglist.js
+++ private/shopping/shoppinglist.js
@@ -21,6 +21,7 @@ let shoppingLists = [];
 let shoppingList = {};
 let clickedList = {};
 let entry = {};
+let joinedGroup = {};
 /**************************************************************************************************/
 /** RUNTIME                                                                                      **/
 /** Declare additial variables for the application in this section.                              **/
@@ -32,6 +33,58 @@ let entry = {};
 /** FUNCTIONS                                                                                    **/
 /** Put the main logic of the application in functions and declare them in this section.         **/
 /**************************************************************************************************/
+//check group for server send events
+async function checkGroup() {
+  try{
+    const response = await fetch('/api/v1/checkGroup');
+    joinedGroup = await response.json();
+    // if(response.status === 200){
+    // };
+    if(response.status === 200){
+      await fetch(`/api/v1/live/${joinedGroup.id}`);
+    }
+  }catch(error){
+    console.log(error);
+  };
+};
+
+function receiveMessage({ data }) {
+  const message = JSON.parse(data);
+
+  if(message.type === 'online' && message.group.id === joinedGroup.id){
+    console.log(message.group.id)
+    const online = message.info;
+    $alertText.textContent = `${online}`;
+    console.log(message)
+    customAlert();
+    return;
+  }
+
+  if(message.type === 'online' && message.group.id === false){
+    console.log(message)
+    const online = message.info;
+    $alertText.textContent = `${online}`;
+    console.log(message)
+    customAlert();
+    return;
+  }
+
+//info messages todo
+if(message.type === 'ToDo' && message.group.id === joinedGroup.id){
+  $alertText.textContent = `${message.info}`;
+  customAlert();
+  return;
+}
+
+//info messages shopping
+if(message.type === 'shoppingList' && message.group.id === joinedGroup.id){
+$alertText.textContent = `${message.info}`;
+customAlert();
+return;
+}
+}
+const sse = new EventSource('/api/v1/live');
+
 function loadEntries(){
   const request = new XMLHttpRequest();
 
@@ -152,9 +205,10 @@ function createNewEntry(entry){
 /**************************************************************************************************/
 $addButton.addEventListener('click', addEntry);
 $navBtn.addEventListener('click', () => window.location.href = 'shopping.html');
-
+sse.addEventListener('message', receiveMessage);
 /**************************************************************************************************/
 /** SETUP                                                                                        **/
 /** If there are any additional steps to take in order to prepare the app, so use this section.  **/
 /**************************************************************************************************/
 loadEntries();
+checkGroup();
