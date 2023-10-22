9257db2a7eab27a6773069d954850a944d17ffe7
Kevin Oberluggauer <kevin.oberluggauer@gmail.com>
2023-10-22 18:05:47 +0200

    updated note files with the Server Send Events functions.

--- private/notes/notes.js
+++ private/notes/notes.js
@@ -17,6 +17,7 @@ const $notes = document.querySelector('.notes');
 const $navBtn = document.querySelector('.navBtn');
 let notes = [];
 
+let joinedGroup = {};
 /**************************************************************************************************/
 /** RUNTIME                                                                                      **/
 /** Declare additial variables for the application in this section.                              **/
@@ -28,6 +29,59 @@ let notes = [];
 /** FUNCTIONS                                                                                    **/
 /** Put the main logic of the application in functions and declare them in this section.         **/
 /**************************************************************************************************/
+//check group for server send events
+async function checkGroup() {
+  try{
+    const response = await fetch('/api/v1/checkGroup');
+    joinedGroup = await response.json();
+    // if(response.status === 200){
+    // };
+    if(response.status === 200){
+      await fetch(`/api/v1/live/${joinedGroup.id}`);
+    }
+  }catch(error){
+    console.log(error);
+  };
+};
+
+const sse = new EventSource('/api/v1/live');
+
+function receiveMessage({ data }) {
+  const message = JSON.parse(data);
+
+  if(message.type === 'online' && message.group.id === joinedGroup.id){
+    console.log(message.group.id)
+    const online = message.info;
+    $alertText.textContent = `${online}`;
+    console.log(message)
+    customAlert();
+    return;
+  }
+
+  if(message.type === 'online' && message.group.id === false){
+    console.log(message)
+    const online = message.info;
+    $alertText.textContent = `${online}`;
+    console.log(message)
+    customAlert();
+    return;
+  }
+
+  //info messages todo
+if(message.type === 'ToDo' && message.group.id === joinedGroup.id){
+  $alertText.textContent = `${message.info}`;
+  customAlert();
+  return;
+}
+
+//info messages shopping
+if(message.type === 'shoppingList' && message.group.id === joinedGroup.id){
+$alertText.textContent = `${message.info}`;
+customAlert();
+return;
+}
+};
+
 
 function loadNotes(){
   const request = new XMLHttpRequest();
@@ -163,9 +217,10 @@ function createNewNote(note){
 $addButton.addEventListener('click', addNote);
 $navBtn.addEventListener('click', () => window.location.href = '../home.html');
 
-
+sse.addEventListener('message', receiveMessage);
 /**************************************************************************************************/
 /** SETUP                                                                                        **/
 /** If there are any additional steps to take in order to prepare the app, so use this section.  **/
 /**************************************************************************************************/
 loadNotes();
+checkGroup();
