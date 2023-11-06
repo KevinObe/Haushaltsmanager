b15269742cb81545f1fd8758d407befb1c81041d
Kevin Oberluggauer <kevin.oberluggauer@gmail.com>
2023-11-06 18:18:21 +0100

    added image caching to improve server performance

--- endpoints/imgCache.js
+++ endpoints/imgCache.js
@@ -1,28 +1,38 @@
 'use strict';
 
-const fs = require('node:fs/promises');
-const path = require('path');
+const serve = require('../system/library/serve.js');
+const twoWeeksInSeconds = 1209600;
 
-let cachedImg;
-
-endpoints.add('/api/v1/cachedImg', async (request, response, session) => {
+endpoints.add('/public/image.background.(jpg|png)', async (request, response, session) => {
   if(request.method !== 'GET'){
     return 405;
   };
 
-  const imgPath = `coffee-concept-with-differents-types-coffee-coffee-beans-milk-cinnamon-sticks-grey-background-flat-lay copy.jpg`;
+  console.log('public')
 
   try{
-    const img = await fs.readFile(imgPath, 'utf8');
-    const twoWeeksInSeconds = 60;
-    response.setHeader('Cache-Control', `public, max-age=${twoWeeksInSeconds}`);
-    response.setHeader('Content-Type', 'image/jpeg');
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.end(img);
+    response.setHeader('Cache-Control', `max-age=${twoWeeksInSeconds}`);
+    serve(response, 'public/image.background.jpg');
   } catch(error){
     console.log(error);
     response.end('Fehler beim Laden der Datei.');
     return 500;
   }
+});
 
+endpoints.add('/private/image.background.(jpg|png)', async (request, response, session) => {
+  if(request.method !== 'GET'){
+    return 405;
+  };
+
+  console.log('private')
+
+  try{
+    response.setHeader('Cache-Control', `max-age=${twoWeeksInSeconds}`);
+    serve(response, 'private/image.background.jpg');
+  } catch(error){
+    console.log(error);
+    response.end('Fehler beim Laden der Datei.');
+    return 500;
+  }
 });
