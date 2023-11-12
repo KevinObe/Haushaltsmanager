addf5829b66cab9292ffb7ea971b088b6a337e10
Kevin Oberluggauer <kevin.oberluggauer@gmail.com>
2023-11-12 15:25:25 +0100

    deleted unnecessary files from the directorys

--- endpoints/home.server.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-//$button.closest('details');
-// html button tag sucht so lange nach oben bis er den nächsten details tag findet;
-
-
-//Seiten reload:
-//popstate event;
-//History.API;
